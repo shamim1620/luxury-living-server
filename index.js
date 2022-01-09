59aeadc6886761a8b0705e083e64dd5e05d704d1
shamim1620 <mdshamimahmed2025@gmail.com>
2022-01-09 17:28:57 +0600

    post message nad service

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const { MongoClient } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config()
 const app = express();
+const fileUpload = require('express-fileupload');
 const port = process.env.PORT || 5000
 
 //midleware
 app.use(cors());
 app.use(express.json());
+app.use(fileUpload())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iwf59.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -15,7 +17,36 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect();
-        console.log('database connect sussesfully');
+        const database = client.db('luxury_living');
+        const serviceCollection = database.collection('services');
+        const messageCollection = database.collection('messages');
+
+
+        app.post('/services', async (req, res) => {
+            const serviceTitle = req.body.serviceTitle;
+            const description = req.body.description;
+            const pic = req.files.image;
+            const picData = pic.data;
+            const encodedPic = picData.toString('base64');
+            const imageBuffer = Buffer.from(encodedPic, 'base64');
+            const service = {
+                serviceTitle,
+                description,
+                image: imageBuffer
+            }
+            const result = await serviceCollection.insertOne(service);
+
+            res.json(result);
+        })
+
+        app.post('/messages', async (req, res) => {
+            const message = req.body;
+            console.log('api is hitted', message);
+
+            const result = await messageCollection.insertOne(message);
+
+            res.json(result);
+        })
 
     }
     finally {
